db2fe0f3a08ebb0fc882dae67fa7505181a91630
Andrew Pilipenko <Cib0rg@users.noreply.github.com>
2024-12-02 11:37:53 +0300

    Update 121-isl.sh

--- scripts/build/companion_libs/121-isl.sh
+++ scripts/build/companion_libs/121-isl.sh
@@ -14,7 +14,7 @@ if [ "${CT_ISL}" = "y" ]; then
 # Download ISL
 do_isl_get() {
     CT_GetFile "isl-${CT_ISL_VERSION}" \
-        http://isl.gforge.inria.fr
+        https://web.archive.org/web/20210701145459/http://isl.gforge.inria.fr
 }
 
 # Extract ISL
